ffdab97ef00da968e6a1072adee2db35917c1dcc
Dave Malvin <dm.enterprienur@gmail.com>
2023-11-14 02:40:10 +0700

    update script to get the dns type

--- script/list_blocked_domain.sh
+++ script/list_blocked_domain.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
 outputfile="/home/back_api/block-list"
+tempfile="/home/back_api/temp-blocklist"
 
 if [ ! -e "$outputfile" ]; then 
 	touch $outputfile
+else
+	rm -f $outputfile
+	touch $outputfile
 fi
 
-cat /etc/bind/db.blocked.rpz | awk '$3=="CNAME" {print}' > $outputfile
-cat /etc/bind/db.ads.rpz | awk '$3=="CNAME" {print}' >> $outputfile
+if [ ! -e "$tempfile" ]; then
+	touch $tempfile
+else
+	rm -f $tempfile
+	touch $tempfile
+fi
 
-cat $outputfile | awk '$1!="^*." {print $1}'
+cat /etc/bind/db.blocked.rpz | awk '$4=="0.0.0.0" {print}' > $outputfile
+cat /etc/bind/db.ads.rpz | awk '$4=="0.0.0.0" {print}' >> $outputfile
 
+while read line; do
+		domains=$(echo $line | cut -d " " -f 1 )
+		types=$(echo $line | cut -d " " -f 3 )
+		echo "$domains,$types" >> $tempfile
+done < $outputfile
+
+cat $tempfile
+
+rm $tempfile
 rm $outputfile
