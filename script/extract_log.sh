90d8d5e6de8a712c7edee68612d3aa509ff94dfb
Dave Malvin <dm.enterprienur@gmail.com>
2023-11-18 00:22:50 +0700

    fixing extract log

--- script/extract_log.sh
+++ script/extract_log.sh
@@ -15,10 +15,12 @@ extract () {
 					type=$( echo $line | cut -d " " -f 12 )
 					echo $type_log,$date,$times,$client,$query,$type'|' >> $outputsfile
 		elif [[ $line =~ "query-error"  ]]; then
-			echo $type_log,$date,$times,$client,"Blocked By Client Filtering"'|' >> $outputsfile
+			query=$( echo $line | cut -d " " -f 8 )
+			echo $type_log,$date,$times,$client,$query,"error","Blocked By Client Filtering"'|' >> $outputsfile
 		elif [[ $line =~ "rpz" ]]; then
 			querys=$( echo $line | cut -d " " -f 8 | cut -d " " -f 1 | cut -d "(" -f 2)
-			echo $type_log,$date,$times,$client,$query'|' >> $outputsfile
+			zone=$(echo $line | cut -d " " -f 15)
+			echo $type_log,$date,$times,$client,$query,"RPZ","Rewrite via "$zone'|' >> $outputsfile
 		fi
 	done < $file
 }
