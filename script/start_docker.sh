7a2c0f070503d0dfc719fa4124236e2df3b97097
Dave Malvin <dm.enterprienur@gmail.com>
2024-01-13 12:36:11 +0700

    change sed

--- script/start_docker.sh
+++ script/start_docker.sh
@@ -18,9 +18,9 @@ cd /home/webScript
 
 ./complete_add_domain.sh db.ads.rpz doubleclick.net 
 
-sed -i -e "s/listen_addresses = .*/listen_addresses = ['127.0.0.1:5353']/" /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-sed -i -e "s|server_names = .*/server_names = ['cloudflare']/" /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-
-dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml &
-
 /usr/sbin/named -c /etc/bind/named.conf -u bind -f
+
+sed -i -e "s/listen_addresses = .*/listen_addresses = \['127.0.0.1:5353'\]/" /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+sed -i -e "s/server_names = .*/server_names = \['cloudflare'\]/" /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+
+/sbin/dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml
