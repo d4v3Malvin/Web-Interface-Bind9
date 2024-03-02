e8a0ebb3769474f6832993b5f073c00615e552df
dave <dave>
2024-03-02 17:51:24 +0000

    applying datetime filter on dns-log

--- Frontend/src/components/Dns-trafficTable.vue
+++ Frontend/src/components/Dns-trafficTable.vue
@@ -11,11 +11,15 @@
                         <div class="flex flex-col justify-center items-center col-span-2">
                             <Multiselect class="w-full" v-model="selectedcat" :options="category" />
                         </div>
-                        <div class="col-span-3">
-                            <div id="test"></div>
-                        </div>
                         <div class="flex flex-col justify-center items-center col-span-2">
                             <input class="w-4/5 h-3/5 text-l" placeholder="Search" name="query" type="text" v-model="searchQuery">
+                        </div>
+                        <div class="w-full flex flex-col justify-center items-center col-span-3">
+                            <div class="w-full" id="daterange">
+                                <input class="w-2/6 text-l" style="margin-right: calc(5%);" v-model="datestart" type="text" ref="datestart">
+                                <input class="w-2/6 text-l" v-model="datefinish" type="text" ref="datedone">
+                                <button @click="checkval()" style="margin-left: calc(5%);" class="w-1/6 bg-purple-200 rounded-md">Search</button> 
+                            </div>
                         </div>
                         <div class="flex flex-col justify-center items-center">
                             <button @click="refreshlist()" class="h-3/4 w-3/4 bg-purple-200 rounded-md">Refresh</button>
@@ -71,6 +75,9 @@
 <script>
     import axios from 'axios'
     import Multiselect from '@vueform/multiselect'
+    import { DateRangePicker } from 'vanillajs-datepicker'
+
+    let daterangeone
 
     let max = 10
     let start = 1
@@ -111,7 +118,11 @@
                 prevSearchQuery: '',
                 totalfilterdata: 0,
                 topsuccessdomain: [],
-                topblockeddomain: []
+                topblockeddomain: [],
+                datestart: '',
+                prevdatestart: '',
+                datefinish: '',
+                prevdatefinish: ''
             }
         },
         computed: {
@@ -154,7 +165,15 @@
         },
         mounted() {
         },
-        created() {1
+        updated() {
+            this.$nextTick(() => {
+                if (document.getElementById('daterange')){
+                    daterangeone = new DateRangePicker(document.getElementById('daterange'),{format : 'dd/mm/yyyy'})
+                    daterangeone.setDates( this.datestart, this.datefinish )
+                }
+            })
+        },
+        created() {
             this.fetchData()
         },
         watch: {
@@ -199,8 +218,14 @@
                 let pagedata;
 
                 if (this.searchQuery != this.prevSearchQuery) {
-                        this.currentpage = 1
-                        this.prevSearchQuery = this.searchQuery // Store the previous search query
+                    this.currentpage = 1
+                    this.prevSearchQuery = this.searchQuery // Store the previous search query
+                }
+
+                if (this.datestart != this.prevdatestart || this.datefinish != this.prevdatefinish) {
+                    this.currentpage = 1
+                    this.prevdatestart = this.datestart
+                    this.prevdatefinish = this.datefinish
                 }
 
                 if (this.searchQuery.toString().trim().length == 0){
@@ -235,6 +260,27 @@
                     pagedata =  filtered
                 }
 
+                if (this.datestart.length > 0 && this.datefinish.length > 0){
+                    let date_min = this.datestart.split('/')
+                    let date_min_epoch = new Date(date_min[2],date_min[1],date_min[0]).getTime()
+                    let date_max = this.datefinish.split('/')
+                    let date_max_epoch = new Date(date_max[2],date_max[1],date_max[0]).getTime()
+                    pagedata = pagedata.filter((data) => {
+                        let date_array = data.date.split('/')
+                        let data_epoch = new Date(date_array[2],date_array[1],date_array[0]).getTime()
+                        if (data_epoch >= date_min_epoch && data_epoch <= date_max_epoch){
+                            return (
+                                data.type ||
+                                data.domain || 
+                                data.ip_source ||
+                                data.dns_type || 
+                                data.date ||
+                                data.time
+                            )
+                        }
+                    })
+                }
+                
                 return pagedata
             },
             invokeFirst(){
@@ -295,6 +341,12 @@
                         this.refreshlist()
                     })
                 }
+            },
+            checkval(){
+                let dates = daterangeone.getDates('dd/mm/yyyy')
+                this.datestart = dates[0]
+                this.datefinish = dates[1]
+                this.refreshlist()
             }
         },
         beforeUnmount() {
@@ -307,3 +359,4 @@
 </script>
 
 <style src="@vueform/multiselect/themes/default.css"></style>
+<style src="vanillajs-datepicker/css/datepicker-foundation.css"></style>
