1b7ca4774a56a0d2771603e0be5b3300413cedd9
dave <dave>
2024-03-13 17:50:56 +0000

    change repo mongo to support ubuntu 20.0

--- startup.sh
+++ startup.sh
@@ -24,7 +24,7 @@ if ! which mongod > /dev/null; then
     curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
     sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
     --dearmor > /dev/null
-    echo -n "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+    echo -n "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
     app+="mongodb-org "
 fi
 if ! which nodejs >/dev/null; then
