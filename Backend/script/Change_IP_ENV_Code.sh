890fc444c3c13048dbadbf61b2e41c3786b6093a
dave <dave>
2024-04-23 11:15:22 +0000

    Change some minor things on changing ip on frontend

--- Backend/script/Change_IP_ENV_Code.sh
+++ Backend/script/Change_IP_ENV_Code.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 repo_path=$1
+status=$2
 
-ip_local=$(hostname -I | awk '{print $1}')
+if [[ $status == "public" ]]; then
+    ip_local=$3
+else
+    ip_local=$(hostname -I | awk '{print $1}')
+fi
+
 
 sed -i -e "s/VUE_APP_HOST_API=.*/VUE_APP_HOST_API='$ip_local'/" "$repo_path/Frontend/.env"
